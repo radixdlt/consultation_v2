a8e593df69c94150ca1ea5b1c2ec08d9df26b8c7
gguuttss <octopusxrd@gmail.com>
2026-02-19 08:57:02 +0100

    chore: format dex addresses

--- apps/vote-collector/src/vote-calculation/dex/constants/addresses.ts
+++ apps/vote-collector/src/vote-calculation/dex/constants/addresses.ts
@@ -200,49 +200,283 @@ export const OCISWAP_PRECISION_POOLS_V2: readonly PrecisionPoolConfig[] = [
 /** All pool-unit-based (fungible LP) pools across all DEXes. */
 export const POOL_UNIT_POOLS: readonly PoolUnitPoolConfig[] = [
   // Ociswap basic pools
-  { name: 'Ociswap Basic: EARLY/XRD', poolAddress: 'pool_rdx1c5hm2rt67scp22pq6tpkfg6cd22g0wwz88065wsy9gdfnd86sv3t4t', lpResourceAddress: 'resource_rdx1t5362v5zqsfkfe38uyl368edpsdm23u5g69qt55jn0ye8nf6umnnv9' },
-  { name: 'Ociswap Basic: OCI/XRD', poolAddress: 'pool_rdx1ckyg8aujf09uh8qlz6asst75g5w6pl6vu8nl6qrhskawcndyk6585y', lpResourceAddress: 'resource_rdx1th7ew2u9c9t00xhk34efm9uj8zxnme48h4ypuerv5uu4ftz8j82gdm' },
-  { name: 'Ociswap Basic: ILIS/XRD', poolAddress: 'pool_rdx1ck0daslg9anw64t5ytq0g4svmuj85jwvrrhgz2005exh8gt6qxle4w', lpResourceAddress: 'resource_rdx1t4vvunhvl24nrc8hh99dujuumyllvvsurvu72keaeh74e25358nhah' },
-  { name: 'Ociswap Basic: WEFT/XRD', poolAddress: 'pool_rdx1ck5w5vnm6qwrmcp4way3wtyjztk7armjea3xc5xaktlk9r4gq6s3ee', lpResourceAddress: 'resource_rdx1th5slwxk8x8xs7438ek6kp7kvrz5lxuu823tql4dqvd92q2fzxr3aq' },
+  {
+    name: 'Ociswap Basic: EARLY/XRD',
+    poolAddress:
+      'pool_rdx1c5hm2rt67scp22pq6tpkfg6cd22g0wwz88065wsy9gdfnd86sv3t4t',
+    lpResourceAddress:
+      'resource_rdx1t5362v5zqsfkfe38uyl368edpsdm23u5g69qt55jn0ye8nf6umnnv9'
+  },
+  {
+    name: 'Ociswap Basic: OCI/XRD',
+    poolAddress:
+      'pool_rdx1ckyg8aujf09uh8qlz6asst75g5w6pl6vu8nl6qrhskawcndyk6585y',
+    lpResourceAddress:
+      'resource_rdx1th7ew2u9c9t00xhk34efm9uj8zxnme48h4ypuerv5uu4ftz8j82gdm'
+  },
+  {
+    name: 'Ociswap Basic: ILIS/XRD',
+    poolAddress:
+      'pool_rdx1ck0daslg9anw64t5ytq0g4svmuj85jwvrrhgz2005exh8gt6qxle4w',
+    lpResourceAddress:
+      'resource_rdx1t4vvunhvl24nrc8hh99dujuumyllvvsurvu72keaeh74e25358nhah'
+  },
+  {
+    name: 'Ociswap Basic: WEFT/XRD',
+    poolAddress:
+      'pool_rdx1ck5w5vnm6qwrmcp4way3wtyjztk7armjea3xc5xaktlk9r4gq6s3ee',
+    lpResourceAddress:
+      'resource_rdx1th5slwxk8x8xs7438ek6kp7kvrz5lxuu823tql4dqvd92q2fzxr3aq'
+  },
   // Ociswap flex pools
-  { name: 'Ociswap Flex: ILIS/XRD', poolAddress: 'pool_rdx1c5cyh7lhxly2mxzsmrs4c99vhxt9jzap3gaf7s8h0h68fqlpfht0un', lpResourceAddress: 'resource_rdx1t4qxj7nnm0sra6f6j9jq73erd489hdad6jp92hggtfwgwy9p2mgn76' },
+  {
+    name: 'Ociswap Flex: ILIS/XRD',
+    poolAddress:
+      'pool_rdx1c5cyh7lhxly2mxzsmrs4c99vhxt9jzap3gaf7s8h0h68fqlpfht0un',
+    lpResourceAddress:
+      'resource_rdx1t4qxj7nnm0sra6f6j9jq73erd489hdad6jp92hggtfwgwy9p2mgn76'
+  },
   // CaviarNine HLP
-  { name: 'CaviarNine HLP: LSULP/XRD (HLP)', poolAddress: 'pool_rdx1chmckjpr0ks5lk6h7mqvmrw56wt4w6tsuy6n2jhd8fhr8vc5en5e90', lpResourceAddress: 'resource_rdx1th0f0khh9g8hwa0qtxsarmq8y7yeekjnh4n74494d5zf4k5vw8qv6m' },
+  {
+    name: 'CaviarNine HLP: LSULP/XRD (HLP)',
+    poolAddress:
+      'pool_rdx1chmckjpr0ks5lk6h7mqvmrw56wt4w6tsuy6n2jhd8fhr8vc5en5e90',
+    lpResourceAddress:
+      'resource_rdx1th0f0khh9g8hwa0qtxsarmq8y7yeekjnh4n74494d5zf4k5vw8qv6m'
+  },
   // CaviarNine simple pools
-  { name: 'CaviarNine Simple: REDDICKS/LSULP', poolAddress: 'pool_rdx1chmx480a0crrnaqyg2e6tr7wtqwk5239grzs6ecckcmhqjm3gdmm73', lpResourceAddress: 'resource_rdx1tkjspzkzmhyzxwcrjha3y2aapmg5690vayjehqtfa729jnr88hcaue' },
-  { name: 'CaviarNine Simple: FLOOP/XRD', poolAddress: 'pool_rdx1ch3vyhagpzqll4cu6quafdpkf7lvyuz7ke4z66tuqpxhvtxzd9lvmu', lpResourceAddress: 'resource_rdx1th2pnc0lzgp20wwv2r22knjn32ntvecapws6v7z644c0d3rzz0fvng' },
-  { name: 'CaviarNine Simple: hUSDC/XRD', poolAddress: 'pool_rdx1c5dcv0r8tz0tzw8radv3grwvdj6jkya84c93k30mqmx70tyatlye0n', lpResourceAddress: 'resource_rdx1tk9hawstw3k86c7qynvvr5tssttnsy4uurkz7d36fkz8cug9yw9925' },
-  { name: 'CaviarNine Simple: hETH/XRD', poolAddress: 'pool_rdx1chcefkz8qqlhl4tk6vm2ftwh7qmht8yru5cxwl0e5r444tw86vzjwd', lpResourceAddress: 'resource_rdx1t5qpw4hf8k60mvn708c46rm7wu8st7kaqwy98nkf987fa2w7ue8kyz' },
-  { name: 'CaviarNine Simple: hwBTC/XRD', poolAddress: 'pool_rdx1ck3ckkse8g2ct0ep4gcymctkfs56ff37lfwlg4w3ehwvgz64evlhf5', lpResourceAddress: 'resource_rdx1thzkkqkeye5qzp5p4nweux47v2elz3v693dg6z20q2ayxaxmdjy52h' },
-  { name: 'CaviarNine Simple: hSOL/XRD', poolAddress: 'pool_rdx1ckg7s8tlluauzxcv8axfl5l4adgzuwan40s7dcmkmwzkc87qletaj9', lpResourceAddress: 'resource_rdx1tkjrw4dr0fyazpu9lql307xv26hy7a8p39m0f6de7ykk2um0965x6f' },
-  { name: 'CaviarNine Simple: hSOL/XRD (v2)', poolAddress: 'pool_rdx1c5dedvmk9nc6pzdk4x00l0r7aut4v2sw9r8lvwlau9rgnumqdvkzce', lpResourceAddress: 'resource_rdx1tksrs4q8f0jv7uq5cnmv2n3snn3vvn62rmp67vthwg25qpaj9vhm88' },
-  { name: 'CaviarNine Simple: hETH/XRD (v2)', poolAddress: 'pool_rdx1ckw50qh8gry85hvq4ctdf95edns7gzeue4jx305td8cgttm8ljutd2', lpResourceAddress: 'resource_rdx1tht9kj9vpdwhny77dn73lqx4ca2tut9yc5q752mhvgtcdhfzyzzn2s' },
-  { name: 'CaviarNine Simple: hUSDC/XRD (v2)', poolAddress: 'pool_rdx1ckgx9waann7djd7frcqevjxq7fh8tfdr5qvyz6ux7s0jkn03mgekkg', lpResourceAddress: 'resource_rdx1t57hslzkmwlcz4fgy6p5xph7v83y6x6n8wjwkn0fphy8cvh6jyntyf' },
-  { name: 'CaviarNine Simple: hwBTC/XRD (v2)', poolAddress: 'pool_rdx1ckp8p90n89qjttl5qzyu6a4dugmxuxkxax4rfsye5fhp6j97jxxvj5', lpResourceAddress: 'resource_rdx1t5x5jmvtwzz9wupx3gcatmq8e2d34kpk364jkhk9ydvwp5ag8t76xc' },
-  { name: 'CaviarNine Simple: hBNB/XRD', poolAddress: 'pool_rdx1c5v5l4ltfy2sgwel8a5x3dte82xgfchky5n8ppkxlm29ngm5hq6qaz', lpResourceAddress: 'resource_rdx1thjv6ex5k66vylwaj59pjvrceg4k0ncf44hsu9aa8rqwgn37vkz7qm' },
+  {
+    name: 'CaviarNine Simple: REDDICKS/LSULP',
+    poolAddress:
+      'pool_rdx1chmx480a0crrnaqyg2e6tr7wtqwk5239grzs6ecckcmhqjm3gdmm73',
+    lpResourceAddress:
+      'resource_rdx1tkjspzkzmhyzxwcrjha3y2aapmg5690vayjehqtfa729jnr88hcaue'
+  },
+  {
+    name: 'CaviarNine Simple: FLOOP/XRD',
+    poolAddress:
+      'pool_rdx1ch3vyhagpzqll4cu6quafdpkf7lvyuz7ke4z66tuqpxhvtxzd9lvmu',
+    lpResourceAddress:
+      'resource_rdx1th2pnc0lzgp20wwv2r22knjn32ntvecapws6v7z644c0d3rzz0fvng'
+  },
+  {
+    name: 'CaviarNine Simple: hUSDC/XRD',
+    poolAddress:
+      'pool_rdx1c5dcv0r8tz0tzw8radv3grwvdj6jkya84c93k30mqmx70tyatlye0n',
+    lpResourceAddress:
+      'resource_rdx1tk9hawstw3k86c7qynvvr5tssttnsy4uurkz7d36fkz8cug9yw9925'
+  },
+  {
+    name: 'CaviarNine Simple: hETH/XRD',
+    poolAddress:
+      'pool_rdx1chcefkz8qqlhl4tk6vm2ftwh7qmht8yru5cxwl0e5r444tw86vzjwd',
+    lpResourceAddress:
+      'resource_rdx1t5qpw4hf8k60mvn708c46rm7wu8st7kaqwy98nkf987fa2w7ue8kyz'
+  },
+  {
+    name: 'CaviarNine Simple: hwBTC/XRD',
+    poolAddress:
+      'pool_rdx1ck3ckkse8g2ct0ep4gcymctkfs56ff37lfwlg4w3ehwvgz64evlhf5',
+    lpResourceAddress:
+      'resource_rdx1thzkkqkeye5qzp5p4nweux47v2elz3v693dg6z20q2ayxaxmdjy52h'
+  },
+  {
+    name: 'CaviarNine Simple: hSOL/XRD',
+    poolAddress:
+      'pool_rdx1ckg7s8tlluauzxcv8axfl5l4adgzuwan40s7dcmkmwzkc87qletaj9',
+    lpResourceAddress:
+      'resource_rdx1tkjrw4dr0fyazpu9lql307xv26hy7a8p39m0f6de7ykk2um0965x6f'
+  },
+  {
+    name: 'CaviarNine Simple: hSOL/XRD (v2)',
+    poolAddress:
+      'pool_rdx1c5dedvmk9nc6pzdk4x00l0r7aut4v2sw9r8lvwlau9rgnumqdvkzce',
+    lpResourceAddress:
+      'resource_rdx1tksrs4q8f0jv7uq5cnmv2n3snn3vvn62rmp67vthwg25qpaj9vhm88'
+  },
+  {
+    name: 'CaviarNine Simple: hETH/XRD (v2)',
+    poolAddress:
+      'pool_rdx1ckw50qh8gry85hvq4ctdf95edns7gzeue4jx305td8cgttm8ljutd2',
+    lpResourceAddress:
+      'resource_rdx1tht9kj9vpdwhny77dn73lqx4ca2tut9yc5q752mhvgtcdhfzyzzn2s'
+  },
+  {
+    name: 'CaviarNine Simple: hUSDC/XRD (v2)',
+    poolAddress:
+      'pool_rdx1ckgx9waann7djd7frcqevjxq7fh8tfdr5qvyz6ux7s0jkn03mgekkg',
+    lpResourceAddress:
+      'resource_rdx1t57hslzkmwlcz4fgy6p5xph7v83y6x6n8wjwkn0fphy8cvh6jyntyf'
+  },
+  {
+    name: 'CaviarNine Simple: hwBTC/XRD (v2)',
+    poolAddress:
+      'pool_rdx1ckp8p90n89qjttl5qzyu6a4dugmxuxkxax4rfsye5fhp6j97jxxvj5',
+    lpResourceAddress:
+      'resource_rdx1t5x5jmvtwzz9wupx3gcatmq8e2d34kpk364jkhk9ydvwp5ag8t76xc'
+  },
+  {
+    name: 'CaviarNine Simple: hBNB/XRD',
+    poolAddress:
+      'pool_rdx1c5v5l4ltfy2sgwel8a5x3dte82xgfchky5n8ppkxlm29ngm5hq6qaz',
+    lpResourceAddress:
+      'resource_rdx1thjv6ex5k66vylwaj59pjvrceg4k0ncf44hsu9aa8rqwgn37vkz7qm'
+  },
   // DefiPlaza pools
-  { name: 'DefiPlaza Base: xUSDC/XRD', poolAddress: 'pool_rdx1c5z06xda4gjykyhupj4fjszdfhsye7h3mcsgwe5cvuz2vemwn7yjax', lpResourceAddress: 'resource_rdx1tkdws0nvfwjnn2q62x4gqgelyt4t5z7cn58pwvrtf4zrxtdw2sem8x' },
-  { name: 'DefiPlaza Quote: xUSDC/XRD', poolAddress: 'pool_rdx1ch62axcl22gnmhe5ajtwraukrxstxxqlq5c6p9n2y5qv0pgyqnhfry', lpResourceAddress: 'resource_rdx1t5gr3wsf7jq28fvnpyfg4rwfkewynv67nnqjna9h5f7mwjuwcwegcj' },
-  { name: 'DefiPlaza Base: xUSDT/XRD', poolAddress: 'pool_rdx1c5pvssdmlgjh78anllzszh7alal666ayv8h6at3xmxmmpueqf7at4q', lpResourceAddress: 'resource_rdx1thnmcry6e02x6ja73llm8z6pkrurvrsudgez4ammsp24r0v20rllxt' },
-  { name: 'DefiPlaza Quote: xUSDT/XRD', poolAddress: 'pool_rdx1c4scl7k67czs4e29skz0njvcmx4epmrjk4nkrkvsmt93rug7jcnagf', lpResourceAddress: 'resource_rdx1t5swt0y0u6sdzycg02flamm3e6qljjgvpxeg5p5tw6jl7ssel0x369' },
-  { name: 'DefiPlaza Base: xETH/XRD', poolAddress: 'pool_rdx1ckt7dhmt5gr9vdsgz3p62fm88pm7f69kzzqw2268f3negvgns2xkpa', lpResourceAddress: 'resource_rdx1t5k00sp4jejklp8cx6nw7ecvhz7z07mfexgmdyflgqpflfvzv8v7wd' },
-  { name: 'DefiPlaza Quote: xETH/XRD', poolAddress: 'pool_rdx1c5glrayedmn0utd44pqs8a3x52dw9aklq2g5f9ewxjxtm7xvjmussa', lpResourceAddress: 'resource_rdx1thhth6tseavhurrgae898k9sht29f3yckzr6szct6zgheqdhxkus0t' },
-  { name: 'DefiPlaza Base: xwBTC/XRD', poolAddress: 'pool_rdx1c5xlqz5uc62fzlsyl2f3ql6lx8upc75tdpe4f8cmys83lpqrrul976', lpResourceAddress: 'resource_rdx1t4x7f34hec2jxtay6cvxvcq3skmkg9pwtr98m4dm7qfrvnaddlavgv' },
-  { name: 'DefiPlaza Quote: xwBTC/XRD', poolAddress: 'pool_rdx1cht7hqhcnj2la96cygema5l32xwz26luunr9umlszy3s9gr78ppdzv', lpResourceAddress: 'resource_rdx1th6ftl6twglqfz2s8ref2vr5nfccaeq2878p4996uq5duszkjhp2gl' },
-  { name: 'DefiPlaza Base: XRD/DFP2', poolAddress: 'pool_rdx1chxn0nqj840r78t2ah5agchq4ue9p65q23nc9ckqfe0mmjstq8fyg0', lpResourceAddress: 'resource_rdx1tknxlx2sy23qkg6twvnu3kqcd5l4daacq0n6mdam54upqgx50f4ju8' },
-  { name: 'DefiPlaza Quote: XRD/DFP2', poolAddress: 'pool_rdx1c4547fnprjhlp2m27aycmf8rzrkrfzcck58jt2706r85gpcaeapz7k', lpResourceAddress: 'resource_rdx1t4a5clnxmnctmezaty08cuugfzmj2lezqcjk2szezrfdfl4w4ederu' },
-  { name: 'DefiPlaza Base: hETH/XRD', poolAddress: 'pool_rdx1ckfa8h47ghy8enmz29k6cxgl5x87qy3gkzetsw82fpuvrdzc6563q2', lpResourceAddress: 'resource_rdx1tha0rthe4jgmwuz0074eazu3n8w2v8m5mpx453vq5ux7dqnaxz0y0g' },
-  { name: 'DefiPlaza Quote: hETH/XRD', poolAddress: 'pool_rdx1chawt3wgkpe0jkdhgrysw9dducx3032nse8r9tfl4gsxfqp2z2alx5', lpResourceAddress: 'resource_rdx1t4xldwkew79skplfk3lempg459xhlhzj5xy5r86jfjrud29lpalytd' },
-  { name: 'DefiPlaza Base: hUSDC/XRD', poolAddress: 'pool_rdx1chxzajmur7p67h0uvk7etgnm9m67ptzfv7ysfdvq35ck2zz6zuttqq', lpResourceAddress: 'resource_rdx1t5qsyevr7ry54uxeh9s7nm6wjdan0c8ks63c2dmpdxsdumum2vsl82' },
-  { name: 'DefiPlaza Quote: hUSDC/XRD', poolAddress: 'pool_rdx1c5t2v0jac6sdyv9qs5hur76uc5kjr70sl8dftc5aa92k0tsuc06r04', lpResourceAddress: 'resource_rdx1tkjvn5zek8aj34rmzd9vd5qtr35x4ytlud857cgnq066lzkc0mygzw' },
-  { name: 'DefiPlaza Base: hwBTC/XRD', poolAddress: 'pool_rdx1c5xdcsxjfpled776cl2ln2mdv9svh5sp4z8u3qhv5r5sslnuxul7rs', lpResourceAddress: 'resource_rdx1tkaems6ywyrqrs7vk0fjk87s8sa2n0wcc4zzkyms04nu6mv739xpyd' },
-  { name: 'DefiPlaza Quote: hwBTC/XRD', poolAddress: 'pool_rdx1ckv54hdmtyn308zkm3mq98876puwhpqa3hrn8d27swgspaackvnmu7', lpResourceAddress: 'resource_rdx1t575tme0szzjp78ms6m2h6suale84d9ululvwhfkm0jdw2y9n50c2f' },
-  { name: 'DefiPlaza Base: hUSDT/XRD', poolAddress: 'pool_rdx1ch5zj8tlarxz38kwh4ss3jn5mphd98rssalkmfut6mhzvswezzg3vd', lpResourceAddress: 'resource_rdx1t46hgy2ut87zeu8jfv6k24d8l4s7mjwjdqsd2qnvu644gyc4l7g0xn' },
-  { name: 'DefiPlaza Quote: hUSDT/XRD', poolAddress: 'pool_rdx1ckvu5wqp2pc284yj7jssckueqtcvt72gjjwz4sv4rl5kdtk70etg2n', lpResourceAddress: 'resource_rdx1t45t2dpdgydlrtpxejwm04uvn2dc8p7nmch8ffcd0vf25axd0aptdk' },
-  { name: 'DefiPlaza Base: hSOL/XRD', poolAddress: 'pool_rdx1c4lrpucdyfhe52znmp3qe8jnjmnt25u6s4hhtz9k88ngju0urj5yy4', lpResourceAddress: 'resource_rdx1tkuxyrqa6lzpsmh75v9af55v4q5gu35wxx0wh30lz6uqd265u0hgvq' },
-  { name: 'DefiPlaza Quote: hSOL/XRD', poolAddress: 'pool_rdx1c43fvtx682u76wz6agel9374sa93kzndkjvz7lh4mdqm8x649krvnq', lpResourceAddress: 'resource_rdx1th7zlzjs0elh2pgr3ce4g8k4g4w2l2sfq27se78dghaqhcx6xq3c5w' },
-  { name: 'DefiPlaza Base: hBNB/XRD', poolAddress: 'pool_rdx1ch0aecz95kl9vdvyz6q3zh4p6vcshefwakx3jfs3uk34y8sv8d8uvy', lpResourceAddress: 'resource_rdx1t4d5n87gsq5vg0xlszyvmd8pgxv5a9nyrj7u9frgxcsy2gtgqk2d3h' },
-  { name: 'DefiPlaza Quote: hBNB/XRD', poolAddress: 'pool_rdx1c42dp3amx7el85vv588vxkqjn53rd8np30qyrpf5pk95uaalu057d4', lpResourceAddress: 'resource_rdx1thhctpg6ctkr7pf4axk0p3ysuyu7574essvj8r8m2d7tf4709x9e6z' }
+  {
+    name: 'DefiPlaza Base: xUSDC/XRD',
+    poolAddress:
+      'pool_rdx1c5z06xda4gjykyhupj4fjszdfhsye7h3mcsgwe5cvuz2vemwn7yjax',
+    lpResourceAddress:
+      'resource_rdx1tkdws0nvfwjnn2q62x4gqgelyt4t5z7cn58pwvrtf4zrxtdw2sem8x'
+  },
+  {
+    name: 'DefiPlaza Quote: xUSDC/XRD',
+    poolAddress:
+      'pool_rdx1ch62axcl22gnmhe5ajtwraukrxstxxqlq5c6p9n2y5qv0pgyqnhfry',
+    lpResourceAddress:
+      'resource_rdx1t5gr3wsf7jq28fvnpyfg4rwfkewynv67nnqjna9h5f7mwjuwcwegcj'
+  },
+  {
+    name: 'DefiPlaza Base: xUSDT/XRD',
+    poolAddress:
+      'pool_rdx1c5pvssdmlgjh78anllzszh7alal666ayv8h6at3xmxmmpueqf7at4q',
+    lpResourceAddress:
+      'resource_rdx1thnmcry6e02x6ja73llm8z6pkrurvrsudgez4ammsp24r0v20rllxt'
+  },
+  {
+    name: 'DefiPlaza Quote: xUSDT/XRD',
+    poolAddress:
+      'pool_rdx1c4scl7k67czs4e29skz0njvcmx4epmrjk4nkrkvsmt93rug7jcnagf',
+    lpResourceAddress:
+      'resource_rdx1t5swt0y0u6sdzycg02flamm3e6qljjgvpxeg5p5tw6jl7ssel0x369'
+  },
+  {
+    name: 'DefiPlaza Base: xETH/XRD',
+    poolAddress:
+      'pool_rdx1ckt7dhmt5gr9vdsgz3p62fm88pm7f69kzzqw2268f3negvgns2xkpa',
+    lpResourceAddress:
+      'resource_rdx1t5k00sp4jejklp8cx6nw7ecvhz7z07mfexgmdyflgqpflfvzv8v7wd'
+  },
+  {
+    name: 'DefiPlaza Quote: xETH/XRD',
+    poolAddress:
+      'pool_rdx1c5glrayedmn0utd44pqs8a3x52dw9aklq2g5f9ewxjxtm7xvjmussa',
+    lpResourceAddress:
+      'resource_rdx1thhth6tseavhurrgae898k9sht29f3yckzr6szct6zgheqdhxkus0t'
+  },
+  {
+    name: 'DefiPlaza Base: xwBTC/XRD',
+    poolAddress:
+      'pool_rdx1c5xlqz5uc62fzlsyl2f3ql6lx8upc75tdpe4f8cmys83lpqrrul976',
+    lpResourceAddress:
+      'resource_rdx1t4x7f34hec2jxtay6cvxvcq3skmkg9pwtr98m4dm7qfrvnaddlavgv'
+  },
+  {
+    name: 'DefiPlaza Quote: xwBTC/XRD',
+    poolAddress:
+      'pool_rdx1cht7hqhcnj2la96cygema5l32xwz26luunr9umlszy3s9gr78ppdzv',
+    lpResourceAddress:
+      'resource_rdx1th6ftl6twglqfz2s8ref2vr5nfccaeq2878p4996uq5duszkjhp2gl'
+  },
+  {
+    name: 'DefiPlaza Base: XRD/DFP2',
+    poolAddress:
+      'pool_rdx1chxn0nqj840r78t2ah5agchq4ue9p65q23nc9ckqfe0mmjstq8fyg0',
+    lpResourceAddress:
+      'resource_rdx1tknxlx2sy23qkg6twvnu3kqcd5l4daacq0n6mdam54upqgx50f4ju8'
+  },
+  {
+    name: 'DefiPlaza Quote: XRD/DFP2',
+    poolAddress:
+      'pool_rdx1c4547fnprjhlp2m27aycmf8rzrkrfzcck58jt2706r85gpcaeapz7k',
+    lpResourceAddress:
+      'resource_rdx1t4a5clnxmnctmezaty08cuugfzmj2lezqcjk2szezrfdfl4w4ederu'
+  },
+  {
+    name: 'DefiPlaza Base: hETH/XRD',
+    poolAddress:
+      'pool_rdx1ckfa8h47ghy8enmz29k6cxgl5x87qy3gkzetsw82fpuvrdzc6563q2',
+    lpResourceAddress:
+      'resource_rdx1tha0rthe4jgmwuz0074eazu3n8w2v8m5mpx453vq5ux7dqnaxz0y0g'
+  },
+  {
+    name: 'DefiPlaza Quote: hETH/XRD',
+    poolAddress:
+      'pool_rdx1chawt3wgkpe0jkdhgrysw9dducx3032nse8r9tfl4gsxfqp2z2alx5',
+    lpResourceAddress:
+      'resource_rdx1t4xldwkew79skplfk3lempg459xhlhzj5xy5r86jfjrud29lpalytd'
+  },
+  {
+    name: 'DefiPlaza Base: hUSDC/XRD',
+    poolAddress:
+      'pool_rdx1chxzajmur7p67h0uvk7etgnm9m67ptzfv7ysfdvq35ck2zz6zuttqq',
+    lpResourceAddress:
+      'resource_rdx1t5qsyevr7ry54uxeh9s7nm6wjdan0c8ks63c2dmpdxsdumum2vsl82'
+  },
+  {
+    name: 'DefiPlaza Quote: hUSDC/XRD',
+    poolAddress:
+      'pool_rdx1c5t2v0jac6sdyv9qs5hur76uc5kjr70sl8dftc5aa92k0tsuc06r04',
+    lpResourceAddress:
+      'resource_rdx1tkjvn5zek8aj34rmzd9vd5qtr35x4ytlud857cgnq066lzkc0mygzw'
+  },
+  {
+    name: 'DefiPlaza Base: hwBTC/XRD',
+    poolAddress:
+      'pool_rdx1c5xdcsxjfpled776cl2ln2mdv9svh5sp4z8u3qhv5r5sslnuxul7rs',
+    lpResourceAddress:
+      'resource_rdx1tkaems6ywyrqrs7vk0fjk87s8sa2n0wcc4zzkyms04nu6mv739xpyd'
+  },
+  {
+    name: 'DefiPlaza Quote: hwBTC/XRD',
+    poolAddress:
+      'pool_rdx1ckv54hdmtyn308zkm3mq98876puwhpqa3hrn8d27swgspaackvnmu7',
+    lpResourceAddress:
+      'resource_rdx1t575tme0szzjp78ms6m2h6suale84d9ululvwhfkm0jdw2y9n50c2f'
+  },
+  {
+    name: 'DefiPlaza Base: hUSDT/XRD',
+    poolAddress:
+      'pool_rdx1ch5zj8tlarxz38kwh4ss3jn5mphd98rssalkmfut6mhzvswezzg3vd',
+    lpResourceAddress:
+      'resource_rdx1t46hgy2ut87zeu8jfv6k24d8l4s7mjwjdqsd2qnvu644gyc4l7g0xn'
+  },
+  {
+    name: 'DefiPlaza Quote: hUSDT/XRD',
+    poolAddress:
+      'pool_rdx1ckvu5wqp2pc284yj7jssckueqtcvt72gjjwz4sv4rl5kdtk70etg2n',
+    lpResourceAddress:
+      'resource_rdx1t45t2dpdgydlrtpxejwm04uvn2dc8p7nmch8ffcd0vf25axd0aptdk'
+  },
+  {
+    name: 'DefiPlaza Base: hSOL/XRD',
+    poolAddress:
+      'pool_rdx1c4lrpucdyfhe52znmp3qe8jnjmnt25u6s4hhtz9k88ngju0urj5yy4',
+    lpResourceAddress:
+      'resource_rdx1tkuxyrqa6lzpsmh75v9af55v4q5gu35wxx0wh30lz6uqd265u0hgvq'
+  },
+  {
+    name: 'DefiPlaza Quote: hSOL/XRD',
+    poolAddress:
+      'pool_rdx1c43fvtx682u76wz6agel9374sa93kzndkjvz7lh4mdqm8x649krvnq',
+    lpResourceAddress:
+      'resource_rdx1th7zlzjs0elh2pgr3ce4g8k4g4w2l2sfq27se78dghaqhcx6xq3c5w'
+  },
+  {
+    name: 'DefiPlaza Base: hBNB/XRD',
+    poolAddress:
+      'pool_rdx1ch0aecz95kl9vdvyz6q3zh4p6vcshefwakx3jfs3uk34y8sv8d8uvy',
+    lpResourceAddress:
+      'resource_rdx1t4d5n87gsq5vg0xlszyvmd8pgxv5a9nyrj7u9frgxcsy2gtgqk2d3h'
+  },
+  {
+    name: 'DefiPlaza Quote: hBNB/XRD',
+    poolAddress:
+      'pool_rdx1c42dp3amx7el85vv588vxkqjn53rd8np30qyrpf5pk95uaalu057d4',
+    lpResourceAddress:
+      'resource_rdx1thhctpg6ctkr7pf4axk0p3ysuyu7574essvj8r8m2d7tf4709x9e6z'
+  }
 ]
 
 // ---------------------------------------------------------------------------
